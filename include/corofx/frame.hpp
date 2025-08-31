3a8983d5fa8276bbf7bb28d5d9aa64a63f43e4f2
vchlin <43309580+vchlin@users.noreply.github.com>
2025-08-31 11:51:27 -0700

    Code cleanup (#39)

--- include/corofx/frame.hpp
+++ include/corofx/frame.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <coroutine>
+#include <type_traits>
 #include <utility>
 
 namespace corofx {
@@ -9,6 +10,12 @@ template<typename P = void>
 class frame {
 public:
     explicit frame(std::coroutine_handle<P> data) noexcept : data_{data} {}
+
+    template<typename Q>
+    frame(frame<Q> that) noexcept
+        requires std::is_void_v<P>
+        : data_{std::exchange(that.data_, {})} {}
+
     frame(frame const&) = delete;
     frame(frame&& that) noexcept : data_{std::exchange(that.data_, {})} {}
 
@@ -25,7 +32,9 @@ public:
     }
 
     auto operator*() const noexcept -> std::coroutine_handle<P> { return data_; }
-    auto operator->() const noexcept -> std::coroutine_handle<P> const* { return &data_; }
+
+    auto operator->() const noexcept
+        -> std::coroutine_handle<P> const* requires(not std::is_void_v<P>) { return &data_; }
 
     friend auto swap(frame& left, frame& right) noexcept -> void {
         using std::swap;
@@ -33,40 +42,10 @@ public:
     }
 
 private:
-    friend class frame<>;
+    template<typename>
+    friend class frame;
 
     std::coroutine_handle<P> data_;
-};
-
-template<>
-class frame<> {
-public:
-    template<typename P>
-    frame(frame<P> that) noexcept : data_{std::exchange(that.data_, {})} {}
-    frame(frame const&) = delete;
-    frame(frame&& that) noexcept : data_{std::exchange(that.data_, {})} {}
-
-    ~frame() {
-        if (data_) data_.destroy();
-    }
-
-    auto operator=(frame const&) -> frame& = delete;
-
-    auto operator=(frame&& that) noexcept -> frame& {
-        auto left = std::move(that);
-        swap(left, *this);
-        return *this;
-    }
-
-    auto operator*() const noexcept -> std::coroutine_handle<> { return data_; }
-
-    friend auto swap(frame& left, frame& right) noexcept -> void {
-        using std::swap;
-        swap(left.data_, right.data_);
-    }
-
-private:
-    std::coroutine_handle<> data_;
 };
 
 } // namespace corofx
